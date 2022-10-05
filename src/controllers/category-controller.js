eed8b3923e566f35866835581575a3a2e241dfbe
nikanoza <nikanozadze1010@gmail.com>
2022-10-05 11:41:27 +0400

    refactor: transform getcategories data

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -2,5 +2,11 @@ import Category from "../models/Category.js";
 
 export const getAllCategories = async (req, res) => {
   const data = await Category.find();
+  const newData = data.map(category => {
+    return {
+      id: category.id,
+      name: category.name,
+    }
+  })
   return res.json(data);
 };
