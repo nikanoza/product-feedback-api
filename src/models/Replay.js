46acc7f08e2444dfc9f30e9863c369243398a24f
hilu <nikanozadze1010@gmail.com>
2022-11-04 09:13:03 +0400

    feat: create replay model

---
+++ src/models/Replay.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const replaySchema = new Schema({
+    content: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    feedbackId: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    commentId: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    userId: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    id: {
+        type: Schema.Types.Number,
+        required: true,
+    }
+})
+
+const Replay = model("Replay", replaySchema);
+
+export default Replay;
