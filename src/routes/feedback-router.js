c402b87b92183a546f98b3f77a91fc6390e68e51
hilu <nikanozadze1010@gmail.com>
2022-11-11 00:29:34 +0400

    feat: update feedback route and swagger

--- src/routes/feedback-router.js
+++ src/routes/feedback-router.js
@@ -3,6 +3,7 @@ import {
   addFeedback,
   getAllFeedbacks,
   getSingleFeedback,
+  updateFeedback,
 } from "../controllers/feedback-controller.js";
 
 const feedbackRouter = express.Router();
@@ -10,5 +11,6 @@ const feedbackRouter = express.Router();
 feedbackRouter.get("/feedbacks", getAllFeedbacks);
 feedbackRouter.get("/feedbacks/:id", getSingleFeedback);
 feedbackRouter.post("/feedbacks/new", addFeedback);
+feedbackRouter.put("/feedback", updateFeedback);
 
 export default feedbackRouter;
